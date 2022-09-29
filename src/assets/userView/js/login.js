33527bbe93f9e65973be15f1f868f8ebd3d612bb
KIDKhoaddow <phamdangkhoa.k.i.d.6868@gmail.com>
2022-09-29 11:10:17 +0700

    first commit

--- src/assets/userView/js/login.js
+++
@@ -1,7 +0,0 @@
-
-function openLoginForm(){
-    document.getElementById("buttonSubmit").setAttribute("onclick","loginUserView()")
-}
-function openRegisterForm(){
-    document.getElementById("buttonSubmit").setAttribute("onclick","registerUserView()")
-}
