1ca9437640c694fd48fbeb1828eba72425c9913d
KIDKhoaddow <phamdangkhoa.k.i.d.6868@gmail.com>
2022-10-10 20:44:02 +0700

    commit to pull giang

--- src/app/home-view/form-create/form-create.component.ts
+++ src/app/home-view/form-create/form-create.component.ts
@@ -10,7 +10,6 @@ import {MyErrorStateMatcher} from "../../model/Validate/ErrorStateMatcher";
 import {Blog} from "../../model/blog/blog";
 
 
-
 @Component({
   selector: 'app-form-create',
   templateUrl: './form-create.component.html',
@@ -25,6 +24,7 @@ export class FormCreateComponent implements OnInit {
   maxTittle = 50
 
   blogDTOs?: Blog []
+
   editorStyle = {
     height: '600px'
   }
@@ -35,31 +35,21 @@ export class FormCreateComponent implements OnInit {
   fb: any
   listURL: any[] = []
 
-
-  username = new FormControl('', Validators.required)
-  categories: Category[]
+  categories: Category[]=[]
   category = new FormControl('', Validators.required)
-  title = new FormControl('', [Validators.required , Validators.minLength(this.minTittle) , Validators.maxLength(this.maxTittle)])
-  description = new FormControl('', [Validators.required , Validators.minLength(this.minDescription) , Validators.maxLength(this.maxDescription)] )
-  content = new FormControl('', [Validators.required, Validators.maxLength(100000000000000000)])
+  title = new FormControl(' ', [Validators.required, Validators.minLength(this.minTittle), Validators.maxLength(this.maxTittle)])
+  description = new FormControl(' ', [Validators.required, Validators.minLength(this.minDescription), Validators.maxLength(this.maxDescription)])
+  content = new FormControl(' ', [Validators.required, Validators.maxLength(100000000000000000)])
   picture = ""
-  createAt = new FormControl('')
-  status = new FormControl('')
-  countLike = new FormControl('')
-  updateAt = new FormControl('')
-  titleMatcher=new MyErrorStateMatcher()
+  titleMatcher = new MyErrorStateMatcher()
+  descriptionMatcher = new MyErrorStateMatcher()
 
   formCreateBlog = this.formGroup.group({
-    username: this.username,
     category: this.category,
     title: this.title,
     description: this.description,
     content: this.content,
-    picture: this.picture,
-    createAt: this.createAt,
-    status: this.status,
-    countLike: this.countLike,
-    updateAt: this.updateAt
+    picture: this.picture
 
   })
 
@@ -67,101 +57,35 @@ export class FormCreateComponent implements OnInit {
               private formGroup: FormBuilder,
               private storage: AngularFireStorage,
               private categoryService: CategoryService) {
-    this.categories = []
-    categoryService.findAll().subscribe(result => {
-      this.categories = result
-      console.log(result)
-    })
-    console.log(this.formCreateBlog)
+
   };
 
 
   ngOnInit(): void {
-    this.findAllBlog()
+    this.categories = []
+    this.categoryService.findAll().subscribe(result => {
+      this.categories = result
+      console.log(result)
+    })
+    console.log(this.formCreateBlog)
   }
 
-  findAllBlog() {
-    this.blogsService.findAll().subscribe(value => this.blogDTOs = value)
-
-  }
 
   createBlog() {
 
     let blog: BlogDTO = {
-      username: this.formCreateBlog.value.username,
       category: Number(this.formCreateBlog.value.category),
       title: this.formCreateBlog.value.title,
       description: this.formCreateBlog.value.description,
       content: this.formCreateBlog.value.content,
       picture: this.picture,
-      createAt: this.formCreateBlog.value.createAt,
-      status: this.formCreateBlog.value.status,
-      countLike: Number(this.formCreateBlog.value.countLike),
-      updateAt: this.formCreateBlog.value.updateAt
     }
     console.log(blog)
     this.blogsService.createBlog(blog).subscribe(value => {
-      if (this.selectedImages.length !== 0) {
-        if (this.selectedImages.length <= 4) {
-          console.log(this.selectedImages.length)
-          for (let i = 0; i < this.selectedImages.length; i++) {
-            let selectedImage = this.selectedImages[i];
-            var n = Date.now();
-            const filePath = `Images/${n}`;
-            const fileRef = this.storage.ref(filePath);
-            this.storage.upload(filePath, selectedImage).snapshotChanges().pipe(
-              finalize(() => {
-                fileRef.getDownloadURL().subscribe(url => {
-                  console.log(url)
-                  this.listURL.push(url)
-                  let image = {
-                    name: url,
-                    product: {
-                      id: value.id
-                    }
-                  }
-                  this.blogsService.saveImage(image).subscribe(() => {
-                    console.log("Create Successfully")
-                  })
-                });
-              })
-            ).subscribe()
-          }
-        } else if (this.selectedImages.length > 4) {
-          console.log(this.selectedImages.length)
-          for (let i = 0; i < 4; i++) {
-            let selectedImage = this.selectedImages[i];
-            var n = Date.now();
-            const filePath = `Images/${n}`;
-            const fileRef = this.storage.ref(filePath);
-            this.storage.upload(filePath, selectedImage).snapshotChanges().pipe(
-              finalize(() => {
-                fileRef.getDownloadURL().subscribe(url => {
-                  console.log(url)
-                  this.listURL.push(url)
-                  let image = {
-                    name: url,
-                    product: {
-                      id: value.id
-                    }
-                  }
-                  this.blogsService.saveImage(image).subscribe(() => {
-                    console.log("Create Successfully")
-                  })
-                });
-              })
-            ).subscribe()
-          }
-        }
-      }
       console.log(value)
-    }, error => {
     })
   }
 
-  test() {
-    console.log(this.category)
-  }
 
   createImage() {
     if (this.selectedImages.length !== 0) {
@@ -196,7 +120,6 @@ export class FormCreateComponent implements OnInit {
     }
     this.createImage();
   }
-
 
 
 }
